e396ec231e08c24023fcbdb9ca52360bea99da5c
R-Brewster <rbrewster@ucmerced.edu>
2018-01-20 01:06:37 -0800

    Added modal functionality

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function initialize() {
     //Add click handler
     $("#scroll_up_glyph").on('click', scrollUp);
     $("#scroll_down_glyph").on('click', scrollDown);
+    $(".topic_tile").on('click', modalData);
 }
 
 function scrollUp() {
@@ -12,4 +13,12 @@ function scrollUp() {
 
 function scrollDown() {
     $("#development_text")[0].scrollTop += 100;
+}
+
+function modalData(event) {
+        var tile = $(event.target);
+        var topic = tile.data('whatever')
+        // modal.find('.modal-title').text('test' + topic)
+        $("#about_modal_title").text(topic)
+        $("#about_modal").modal('show')
 }
