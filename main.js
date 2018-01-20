ad6e881b2baa177154c59943cc6a7a9436234b04
R-Brewster <rbrewster@ucmerced.edu>
2018-01-20 12:49:02 -0800

    Adding employment text

--- main.js
+++ main.js
@@ -22,3 +22,15 @@ function modalData(event) {
         $("#about_modal_title").text(topic)
         $("#about_modal").modal('show')
 }
+
+function modalText(topic) {
+    var modalText = {
+        Background: 
+            'I have been fortunate enough to have lived in California my whole life. I was born in the foothills of San Diego County, a place full of oak trees and creeks, and plenty of wildlife for a curious child to explore. My family includes parents who have both had careers as computer engineers and a twin brother, who was dagged along on many of the aforementioned backyard wilderness adventures.',
+        Education: 
+            'I wanted to stay in California while earning my Bacherlors, and since I also wanted to study Biology, going to a research university made sense. So I attended UC Merced, the newest of the UC system and the one with an exciting pioneering energy I could not resist. My degreee ended up being a B.S. in Biology with an emphasis in ecology and evolutionary biology.' <br> 'My more recent educational experience was attending Learning Fuze, a web development school in Irvine. I was a part of their full-immersion course from October 2017 to December 2017. We were taught not only how to code in Javascript with methodologies such as OOP and MVC, but also in an agile structured environment with such technologies as Github. Several of my projects are featured below.',
+        Employment: 
+            'My first job was a very unique one, I was a summer Park Ranger Intern in Yosemite National Park. I interned with the rangers of the interpretation branch, which means that I led tours around the park, staffed visitor centers, and conducted campfire talks. I was able to really develop my communication skills, which have proven invaluable since.' <br> 'My student job for the majority of college was as a marketing intern for the admissions department. I once again was able to improve my communication skills, as well as my writing and creative abilites as I produced content for various social media accounts. This was also my chance to learn new technologies as I dabbled in video editing, publication creation, and marketing campaigns.' <br> 'Upon graduating from college, I sort of continued my student marketing position as a recruiter',
+        Hobbies: Test,
+    }
+}
