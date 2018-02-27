ee6aed8d259aec2817152ad63b7b147dae3a5806
R-Brewster <rbrewster@ucmerced.edu>
2018-02-26 17:13:56 -0800

    Php mailer working now

--- assets/php_mailer/mail_handler.php
+++ assets/php_mailer/mail_handler.php
@@ -1,57 +1,44 @@
 <?php
 require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+// $mail->SMTPDebug = 3;           // 3 is Enable verbose debug output. Change to 0 to disable debugging output.
 
-//$_POST('name');
-//$_POST('email');
-//$_POST('phone');
-//$_POST('message');
-//$_POST('submit');
-//
-$message = [];
-$output = [
-    'success' => null,
-    'message' => [],
+$message = [
+    // 'email' => $_POST['email'],
+    // 'subject' => $_POST['subject'],
+    // 'body' => $_POST['message']
 ];
+
+$output =[
+    'success' => null,
+    'message' => []
+];
+
+//Sanitize name field
 $message['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
 if(empty($message['name'])){
     $output['success'] = false;
     $output['messages'][] = 'missing name key';
 }
+//Validate email field
 $message['email'] = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 if(empty($message['email'])){
     $output['success'] = false;
     $output['messages'][] = 'invalid email key';
 }
+//Sanitize message
 $message['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 if(empty($message['message'])){
     $output['success'] = false;
     $output['messages'][] = 'missing message key';
 }
 
-$message['phone'] = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
-if(empty($message['phone'])){
-    $output['success'] = false;
-    $output['messages'][] = 'missing phone key';
-}
-
-if($output['success']!==null){
-    http_response_code(400);
-    echo json_encode($output);
-    exit();
-}
-
-$message['message'] = nl2br($message['message']);
-
-$mail = new PHPMailer;
-//$mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
-
 $mail->isSMTP();                // Set mailer to use SMTP.
 $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
 $mail->SMTPAuth = true;         // Enable SMTP authentication
 
-
-$mail->Username =EMAIL_USER;   // SMTP username
+$mail->Username = EMAIL_USER;   // SMTP username
 $mail->Password = EMAIL_PASS;   // SMTP password
 $mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
 $mail->Port = 587;              // TCP port to connect to
@@ -62,10 +49,11 @@ $options = array(
         'allow_self_signed' => true
     )
 );
+
 $mail->smtpConnect($options);
 $mail->From = $message['email'];  // sender's email address (shows in "From" field)
 $mail->FromName = $message['name'];   // sender's name (shows in "From" field)
-$mail->addAddress('rlbrewster8tech@gmail.com');  // Add a recipient
+$mail->addAddress("rlbrewster8tech@gmail.com");  // Add a recipient
 //$mail->addAddress('ellen@example.com');                        // Name is optional
 $mail->addReplyTo($message['email'], $message['name']);                          // Add a reply-to address
 //$mail->addCC('cc@example.com');
@@ -75,16 +63,26 @@ $mail->addReplyTo($message['email'], $message['name']);                         
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-//$mail->Subject = $message['subject'];
+$mail->Subject = $_POST['subject'];
 $mail->Body    = $message['message'];
 $mail->AltBody = htmlentities($message['message']);
 
+if ($output['success'] !== null) {
+    http_response_code(400);
+    echo json_encode($output);
+    exit();
+}
+
+$message['message'] = nl2br($message['message']);
+
 if(!$mail->send()) {
     $output['success'] = false;
-    $output['messages'][]=$mail->ErrorInfo;
+    $output['message'][] = $mail->ErrorInfo;
+    // echo 'Message could not be sent.';
+    // echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     $output['success'] = true;
+    // echo 'Message has been sent';
 }
-echo json_encode($output);
-
+print(json_encode($output));
 ?>
